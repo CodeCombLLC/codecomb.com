49b91bbea712327862e1e466f702ad8aa49a8e95
あまみや ゆうこ <1@1234.sh>
2015-11-16 16:01:41 +0800

    implement home page

---
+++ src/CodeComb.WebSite/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+
+namespace CodeComb.WebSite
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseIISPlatformHandler();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
---
+++ src/CodeComb.WebSite/wwwroot/scripts/html5.js
@@ -0,0 +1,7 @@
+﻿(function () {
+    var aHtml5 = ['article', 'aside', 'audio', 'bdi', 'canvas', 'command', 'datalist', 'details', 'figcaption', 'figure', 'footer', 'header', 'hgroup', 'keygen', 'mark', 'meter', 'nav', 'output', 'progress', 'rp', 'rt', 'ruby', 'section', 'source', 'summary', 'time', 'track', 'vedio', 'main'];
+    var iHtml5 = aHtml5.length;
+    for (var i = 0; i < iHtml5; i++) {
+        document.createElement(aHtml5[i]);
+    }
+})();
